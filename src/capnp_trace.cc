f8db354d928fa4da3b00ca097ee976dbbd532fa8
Takeo Kondo <takeo_kondo@mail.toyota.co.jp>
2024-05-01 13:38:22 +0900

    chore: version up to v0.1.0

--- src/capnp_trace.cc
+++ src/capnp_trace.cc
@@ -35,7 +35,7 @@
 
 namespace capnp_trace {
 
-static const char VERSION_STRING[] = "capnp_trace v0.0.1";
+static const char VERSION_STRING[] = "capnp_trace v0.1.0";
 
 static inline std::string GetTimeStamp() {
   struct timespec tp;
